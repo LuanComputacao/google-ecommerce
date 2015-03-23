9f7c6ec768929aa8fcd3e07ad2504f3483f6884d
Renato Dehnhardt <renato.dehnhardt@lojaskd.com.br>
2015-03-23 17:43:03 -0300

    FIX

--- jquery.google.ecommerce.js
+++ jquery.google.ecommerce.js
@@ -1,103 +1,104 @@
 (function ($) {
-        jQuery.fn.googleEcommerce = function (settings) {
-            var config = {};
+    jQuery.fn.googleEcommerce = function (settings) {
+        var config = {};
 
-            if (settings) {
-                $.extend(config, settings);
-            }
+        if (settings) {
+            $.extend(config, settings);
+        }
 
-            var methods = {
-                impression: function (element) {
-                    ga('ec:addImpression', {
-                        'id': element.data('ec-id'),
-                        'name': element.data('ec-name'),
-                        'category': element.data('ec-category'),
-                        'brand': element.data('ec-brand'),
-                        'variant': element.data('ec-variant'),
-                        'list': element.data('ec-list'),
-                        'position': element.data('ec-position'),
-                        'dimension1': element.data('ec-dimension')
-                    });
-                },
-                click: function (element) {
-                    ga('ec:addProduct', {
-                        'id': element.data('ec-id'),
-                        'name': element.data('ec-name'),
-                        'category': element.data('ec-category'),
-                        'brand': element.data('ec-brand'),
-                        'variant': element.data('ec-variant'),
-                        'list': element.data('ec-list'),
-                        'position': element.data('ec-position'),
-                        'dimension1': element.data('ec-dimension')
-                    });
-
-                    ga("ec:setAction", "clickbitch", {"list": element.data('ec-list')});
-                    ga("send", "event", "homepage", "click", "");
-                },
-                addCart: function (element) {
-                    ga("ec:addProduct", {
-                        "id": element.data('ec-id'),
-                        "name": element.data('ec-name'),
-                        "price": element.data('ec-price'),
-                        "brand": element.data('ec-brand'),
-                        "category": element.data('ec-category'),
-                        "variant": element.data('ec-variant'),
-                        "dimension1": element.data('ec-dimension'),
-                        "list": element.data('ec-list'),
-                        "position": element.data('ec-position'),
-                        "quantity": element.data('ec-quantity')
-                    });
-
-                    ga("ec:setAction", "add", {"list": element.data('ec-list')});
-                    ga("send", "event", "detail view", "click", "addToCart");
-                },
-                removeCart: function (element) {
-                    ga("ec:addProduct", {
-                        "id": element.data('ec-id'),
-                        "name": element.data('ec-name'),
-                        "price": element.data('ec-price'),
-                        "brand": element.data('ec-brand'),
-                        "category": element.data('ec-category'),
-                        "variant": element.data('ec-variant'),
-                        "dimension1": element.data('ec-dimension'),
-                        "position": element.data('ec-position'),
-                        "quantity": element.data('ec-quantity')
-                    });
-
-                    ga("ec:setAction", "remove");
-                    ga("send", "event", "detail view", "click", "removeFromCart");
-                }
-            };
-
-            $('[data-ec-click]').on('click', function (e) {
-                var element = $('[data-ec-id="' + $(this).data('ec-id') + '"]')
-                e.stopPropagation();
-
-                switch ($(this).data('ec-click')) {
-                    case 'addCard':
-                        methods.addCart(element);
-                        break;
-
-                    case 'removeCard':
-                        methods.removeCart(element);
-                        break;
-                }
-
-                $('[data-ec-click]').attr('disabled', false);
-            });
-
-            return this.each(function () {
-                var element = $(this);
-
-                methods.impression(element);
-
-                element.on('click', function (e) {
-                    methods.click(element);
-                    e.stopPropagation();
+        var methods = {
+            impression: function (element) {
+                ga('ec:addImpression', {
+                    'id': element.data('ec-id'),
+                    'name': element.data('ec-name'),
+                    'category': element.data('ec-category'),
+                    'brand': element.data('ec-brand'),
+                    'variant': element.data('ec-variant'),
+                    'list': element.data('ec-list'),
+                    'position': element.data('ec-position'),
+                    'dimension1': element.data('ec-dimension')
                 });
-            });
+            },
+            click: function (element) {
+                ga('ec:addProduct', {
+                    'id': element.data('ec-id'),
+                    'name': element.data('ec-name'),
+                    'category': element.data('ec-category'),
+                    'brand': element.data('ec-brand'),
+                    'variant': element.data('ec-variant'),
+                    'list': element.data('ec-list'),
+                    'position': element.data('ec-position'),
+                    'dimension1': element.data('ec-dimension')
+                });
+
+                ga("ec:setAction", "clickbitch", {"list": element.data('ec-list')});
+                ga("send", "event", "homepage", "click", "");
+            },
+            addCart: function (element) {
+                ga("ec:addProduct", {
+                    "id": element.data('ec-id'),
+                    "name": element.data('ec-name'),
+                    "price": element.data('ec-price'),
+                    "brand": element.data('ec-brand'),
+                    "category": element.data('ec-category'),
+                    "variant": element.data('ec-variant'),
+                    "dimension1": element.data('ec-dimension'),
+                    "list": element.data('ec-list'),
+                    "position": element.data('ec-position'),
+                    "quantity": element.data('ec-quantity')
+                });
+
+                ga("ec:setAction", "add", {"list": element.data('ec-list')});
+                ga("send", "event", "detail view", "click", "addToCart");
+            },
+            removeCart: function (element) {
+                ga("ec:addProduct", {
+                    "id": element.data('ec-id'),
+                    "name": element.data('ec-name'),
+                    "price": element.data('ec-price'),
+                    "brand": element.data('ec-brand'),
+                    "category": element.data('ec-category'),
+                    "variant": element.data('ec-variant'),
+                    "dimension1": element.data('ec-dimension'),
+                    "position": element.data('ec-position'),
+                    "quantity": element.data('ec-quantity')
+                });
+
+                ga("ec:setAction", "remove");
+                ga("send", "event", "detail view", "click", "removeFromCart");
+            }
         };
 
-        $('[data-ec=true]').googleEcommerce();
-        ga('send', 'pageview');
-    }(jQuery));
+        $('[data-ec-click]').on('click', function (e) {
+            var element = $('[data-ec-id="' + $(this).data('ec-id') + '"]')
+            e.stopPropagation();
+
+            switch ($(this).data('ec-click')) {
+                case 'addCard':
+                    methods.addCart(element);
+                    break;
+
+                case 'removeCard':
+                    methods.removeCart(element);
+                    break;
+            }
+
+            $('[data-ec-click]').attr('disabled', false);
+        });
+
+        return this.each(function () {
+            var element = $(this);
+
+            methods.impression(element);
+
+            element.on('click', function (e) {
+                methods.click(element);
+                e.stopPropagation();
+            });
+        });
+    };
+
+    ga('require', 'ec');
+    $('[data-ec=true]').googleEcommerce();
+    ga('send', 'pageview');
+}(jQuery));
